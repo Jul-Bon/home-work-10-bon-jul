3d30fd33174b5cd9cf024ec91fcb056b5c76c8cf
Jul Bon <yulija.bon007@gmail.com>
2017-12-23 21:05:08 +0200

    factorial

--- source-js/main.js
+++ source-js/main.js
@@ -5,14 +5,12 @@ function sum (number1, number2) {
 
 console.log(sum(10, 5))
 
-
 //Function for subtraction of numbers
 function deduction (number1, number2) {
   return number1 - number2
 }
 
 console.log(deduction(10, 5))
-
 
 //Function for multiplication of numbers
 function multiple (number1, number2) {
@@ -21,14 +19,12 @@ function multiple (number1, number2) {
 
 console.log(multiple(10, 5))
 
-
 //Function for division of numbers
 function divide (number1, number2) {
   return number1 / number2
 }
 
 console.log(divide(10, 5))
-
 
 //Function of elevation to degree
 function degree (number1, number2) {
@@ -37,14 +33,12 @@ function degree (number1, number2) {
 
 console.log(degree(2, 5))
 
-
 //Function returns the square root of a number
 function root (number1) {
   return Math.sqrt(number1)
 }
 
 console.log(root(8))
-
 
 //Function for calculating a cosine
 function cosine (number1) {
@@ -53,7 +47,6 @@ function cosine (number1) {
 
 console.log(cosine(1))
 
-
 //Function for sinus calculation
 function sinus (number1) {
   return Math.sin(number1)
@@ -61,10 +54,16 @@ function sinus (number1) {
 
 console.log(sinus(1))
 
-
 //Calculation of factorial number
-function factorial (n) {
-  return (n != 1) ? n * factorial(n - 1) : 1
+function factorial(n) {
+  if (n === 0) {
+    return 1
+  }
+  return n * factorial(n - 1)
 }
 
-console.log(factorial(4))
+console.log(factorial(5))
+
+
+
+
